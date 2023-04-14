bdaca976498aeea074fc2bd8ae2214e9dab23f4f
camilogs <camilo.gonzalez.col@gmail.com>
2023-04-14 14:09:26 -0500

    Add connection string

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using webAPI.Services;
+using webAPI.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSqlServer<DbWebAPIContext>(builder.Configuration.GetConnectionString("webAPI"));
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IOpenTaskService, OpenTaskService>();
 
