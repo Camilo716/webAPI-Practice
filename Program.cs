83831271659cf7130a4594e356521071025148a8
camilogs <camilo.gonzalez.col@gmail.com>
2023-04-13 19:17:15 -0500

    Add openTask and category services as dependences applying DIP

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using webAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IOpenTaskService, OpenTaskService>();
 
 var app = builder.Build();
 
